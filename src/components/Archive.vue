7c717d126960abde11ab4379ddeb2a3850f1fff2
Jesús Manuel García Muñoz <jesus@fastmail.es>
2020-05-30 01:02:51 +0200

    makes archive table responsive

--- src/components/Archive.vue
+++ src/components/Archive.vue
@@ -4,42 +4,44 @@
 
     <p v-if="isLoading">Cargando el archivo…</p>
 
-    <table v-if="!isLoading" class="archive-table">
-      <thead>
-        <tr>
-          <th class="archive-table__cell--published-at">Fecha</th>
-          <th class="archive-table__cell--title">Publicación</th>
-          <th class="archive-table__cell--song">Canción</th>
-          <th class="archive-table__cell--artist">Artista</th>
-          <th class="archive-table__cell--album">Álbum</th>
-          <th class="archive-table__cell--year">Año</th>
-          <th class="archive-table__cell--author">Autor</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="post in filteredArchive" :key="post.id">
-          <td class="archive-table__cell--published-at">{{ formattedDate(post.published_at) }}</td>
-          <td class="archive-table__cell--title"><router-link :to="`/archivo/${post.id}`" target="_blank">{{ post.title }}</router-link></td>
-          <td class="archive-table__cell--song">{{ post.metadata_song }}</td>
-          <td class="archive-table__cell--artist">
-            <router-link :to="{ query: filter({ artist: post.metadata_artist }) }">{{ post.metadata_artist }}</router-link>
-            <router-link v-if="artistFilter" :to="{ query: filter({ artist: null }) }" class="archive-table__remove-filter"></router-link>
-          </td>
-          <td class="archive-table__cell--album">
-            <router-link :to="{ query: filter({ album: post.metadata_album }) }">{{ post.metadata_album }}</router-link>
-            <router-link v-if="albumFilter" :to="{ query: filter({ album: null }) }" class="archive-table__remove-filter"></router-link>
-          </td>
-          <td class="archive-table__cell--year">
-            <router-link :to="{ query: filter({ year: post.metadata_year }) }">{{ post.metadata_year }}</router-link>
-            <router-link v-if="yearFilter" :to="{ query: filter({ year: null }) }" class="archive-table__remove-filter"></router-link>
-          </td>
-          <td class="archive-table__cell--author">
-            <router-link :to="{ query: filter({ authorName: post.author_name }) }">{{ post.author_name }}</router-link>
-            <router-link v-if="authorNameFilter" :to="{ query: filter({ authorName: null }) }" class="archive-table__remove-filter"></router-link>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div v-if="!isLoading" class="archive-table-wrapper">
+      <table class="archive-table">
+        <thead>
+          <tr>
+            <th class="archive-table__cell--published-at">Fecha</th>
+            <th class="archive-table__cell--title">Publicación</th>
+            <th class="archive-table__cell--song">Canción</th>
+            <th class="archive-table__cell--artist">Artista</th>
+            <th class="archive-table__cell--album">Álbum</th>
+            <th class="archive-table__cell--year">Año</th>
+            <th class="archive-table__cell--author">Autor</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="post in filteredArchive" :key="post.id">
+            <td class="archive-table__cell--published-at">{{ formattedDate(post.published_at) }}</td>
+            <td class="archive-table__cell--title"><router-link :to="`/archivo/${post.id}`" target="_blank">{{ post.title }}</router-link></td>
+            <td class="archive-table__cell--song">{{ post.metadata_song }}</td>
+            <td class="archive-table__cell--artist">
+              <router-link :to="{ query: filter({ artist: post.metadata_artist }) }">{{ post.metadata_artist }}</router-link>
+              <router-link v-if="artistFilter" :to="{ query: filter({ artist: null }) }" class="archive-table__remove-filter"></router-link>
+            </td>
+            <td class="archive-table__cell--album">
+              <router-link :to="{ query: filter({ album: post.metadata_album }) }">{{ post.metadata_album }}</router-link>
+              <router-link v-if="albumFilter" :to="{ query: filter({ album: null }) }" class="archive-table__remove-filter"></router-link>
+            </td>
+            <td class="archive-table__cell--year">
+              <router-link :to="{ query: filter({ year: post.metadata_year }) }">{{ post.metadata_year }}</router-link>
+              <router-link v-if="yearFilter" :to="{ query: filter({ year: null }) }" class="archive-table__remove-filter"></router-link>
+            </td>
+            <td class="archive-table__cell--author">
+              <router-link :to="{ query: filter({ authorName: post.author_name }) }">{{ post.author_name }}</router-link>
+              <router-link v-if="authorNameFilter" :to="{ query: filter({ authorName: null }) }" class="archive-table__remove-filter"></router-link>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
